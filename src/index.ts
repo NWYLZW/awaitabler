d7cc207e482db8385107e5d4c007d843a82c2d7d
yijie4188 <yijie4188@gmail.com>
2023-07-22 00:49:36 +0800

    Use configure and define context resolve rule

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import * as Self from './index'
+import { configure } from './configure'
 
 export default Self
 
@@ -22,7 +23,10 @@ declare global {
 const middlewares = new Set<Middleware>()
 
 export interface Context {
+  tags: string[]
+  target: string
   schema: string
+  cmd: string
 }
 export interface Middleware {
   (ctx: Context, next: () => Promise<void>): void | Promise<void>
@@ -32,12 +36,55 @@ export function defineMiddleware(mid: Middleware) {
   return () => middlewares.delete(mid)
 }
 
-export function supportFetch() {
+export function supportRequest() {
   defineMiddleware((ctx, next) => {
     if (['http', 'https'].includes(ctx.schema)) {
     }
     return next()
   })
+}
+
+/**
+ * await string EBNF
+ * ```EBNF
+ * tag = '[' , { all characters - '] ' } , '] ' ;
+ * schema = { all characters - ':' } , ':' ;
+ * target = schema , { all characters } ;
+ * await string rule = { tag } , target ;
+ * ```
+ * [POST] [Header.Content-Type=application/json] https://www.com/to/target?query=string#hash
+ * \_____/\_____________________________________/\____/\___________________________________/
+ *  |       |                                      |       |
+ * tag0    tag1                                   schema  cmd
+ * \____________________________________________/\_________________________________________/
+ *                    |                                              |
+ *                   tags                                          target
+ * more examples:
+ * [../] shell:ls -l
+ * [.../] fs://./url/to/file
+ */
+export function resolveContext(str: string) {
+  const ctx: Context = {
+    tags: [],
+    schema: configure.defaultSchema,
+    target: `${configure.defaultSchema}:`,
+    cmd: ''
+  }
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === '[') {
+      const tag = []
+      while (str[i] !== ']') {
+        tag.push(str[i])
+        i++
+      }
+      ctx.tags.push(tag.join(''))
+    } else if (str[i] === ':') {
+      ctx.schema = str.slice(0, i)
+      ctx.target = str.slice(i + 1)
+      break
+    }
+  }
+  return ctx
 }
 
 export function registerString() {
@@ -53,7 +100,9 @@ export function registerString() {
     }) as StringFunction
   }
   String.prototype.then = /** @type {String['then']} */ (function (on0, on1) {
-    return fetch(this.toString()).then(on0, on1)
+    const tags: string[] = []
+    const str = this.toString()
+    return fetch(str).then(on0, on1)
   })
 }
 
