e30f873f597a4fd89dc86d8c576b1276c1d1fbad
yijie4188 <yijie4188@gmail.com>
2023-07-21 23:09:19 +0800

    Remove global copyToClipboard function

--- playground/src/main.tsx
+++ playground/src/main.tsx
@@ -3,15 +3,6 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
-window.copyToClipboard = function (content) {
-  const input = document.createElement('input')
-  input.value = content
-  document.body.appendChild(input)
-  input.select()
-  document.execCommand('copy')
-  document.body.removeChild(input)
-}
-
 window.run = function run(code, lang) {
   document.querySelector<HTMLIFrameElement>('iframe.eval-logs')
     ?.contentWindow
