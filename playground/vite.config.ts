015a5382ffc80baeafa408fb2015a686b1993793
yijie4188 <yijie4188@gmail.com>
2023-07-30 00:29:19 +0800

    Make react and @babel/standalone as external dependency

--- playground/vite.config.ts
+++ playground/vite.config.ts
@@ -2,20 +2,27 @@ import path from 'node:path'
 import { defineConfig } from 'vite'
 
 import react from '@vitejs/plugin-react'
+import { viteExternalsPlugin } from 'vite-plugin-externals'
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(env => ({
   base: '/awaitabler/',
-  plugins: [react()],
+  plugins: [
+    react(),
+    env.mode === 'production' ? viteExternalsPlugin({
+      '@babel/standalone': 'Babel',
+      'react': 'React',
+    }) : null
+  ],
   build: {
     rollupOptions: {
       input: {
         main: path.resolve(__dirname, 'index.html'),
         'eval-logs': path.resolve(__dirname, 'eval-logs.html')
-      }
+      },
     }
   },
   define: {
     MODE: '"esm"'
   }
-})
+}))
