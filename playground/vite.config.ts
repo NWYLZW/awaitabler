255ef3f16c61b3e913f1cd7dd4cc44d9c15d2ce0
yijie4188 <yijie4188@gmail.com>
2023-07-30 00:36:46 +0800

    Import build analysis and make react-dom as external dependency

--- playground/vite.config.ts
+++ playground/vite.config.ts
@@ -4,6 +4,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { viteExternalsPlugin } from 'vite-plugin-externals'
 
+import { visualizer } from 'rollup-plugin-visualizer'
+
 // https://vitejs.dev/config/
 export default defineConfig(env => ({
   base: '/awaitabler/',
@@ -12,6 +14,7 @@ export default defineConfig(env => ({
     env.mode === 'production' ? viteExternalsPlugin({
       '@babel/standalone': 'Babel',
       'react': 'React',
+      'react-dom': 'ReactDOM',
     }) : null
   ],
   build: {
@@ -20,6 +23,7 @@ export default defineConfig(env => ({
         main: path.resolve(__dirname, 'index.html'),
         'eval-logs': path.resolve(__dirname, 'eval-logs.html')
       },
+      plugins: [visualizer()]
     }
   },
   define: {
