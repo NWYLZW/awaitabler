8f57f350a5390404afb1f23719787eefe5f4aad3
yijie4188 <yijie4188@gmail.com>
2023-08-01 23:20:50 +0800

    Refactor widget get and pane init logic

--- playground/src/pages/eval-logs/devtools.ts
+++ playground/src/pages/eval-logs/devtools.ts
@@ -1,25 +1,6 @@
-import * as UI from '//chii/ui/legacy/legacy.js'
+import type * as UI from '//chii/ui/legacy/legacy.js'
 
 import { getFiles } from './files.ts'
-
-class JSOutput extends UI.Widget.Widget {
-  constructor() {
-    super()
-    const text = document.createElement('pre')
-    text.style.cursor = 'text'
-    text.style.userSelect = 'text'
-    text.style.whiteSpace = 'pre-wrap'
-    text.style.margin = '0'
-    text.innerText = ''
-    const [FILES, onFiles] = getFiles()
-    function update(files = FILES) {
-      text.innerText = files.map(({ name, originalText }) => `// @filename:${name}\n${originalText}`).join('\n\n')
-    }
-    update()
-    onFiles(update)
-    this.contentElement.appendChild(text)
-  }
-}
 
 initChii: (async () => {
   localStorage.setItem('panel-selectedTab', JSON.stringify('console'))
@@ -31,19 +12,45 @@ initChii: (async () => {
 
   localStorage.setItem('textEditorIndent', JSON.stringify('  '))
 
-  const devtools = document.querySelector('iframe')!.contentDocument!
+  const devtools = document.querySelector('iframe')!
+  const devtoolsWindow = devtools.contentWindow!
+  const devtoolsDocument = devtools.contentDocument!
 
+  let inited = false
   function init() {
-    // @ts-ignore
-    const chiiWidget = devtools.querySelector('.vbox.flex-auto.split-widget')?.__widget
-    if (chiiWidget) {
-      const tabbedPane = chiiWidget.sidebarWidgetInternal.tabbedPane
-      tabbedPane.appendTab('.js', '.JS', new JSOutput())
+    const importUI = devtoolsWindow
+      // @ts-ignore
+      .importUI as () => typeof UI
+    const realUI = importUI()
+
+    const chiiSidebar = realUI.InspectorView.InspectorView.instance()
+    if (chiiSidebar && !inited) {
+      inited = true
+
+      const tabbedPane = chiiSidebar?.tabbedPane
+      tabbedPane.appendTab('.js', '.JS', new class JSOutput extends realUI.Widget.Widget {
+        constructor() {
+          super()
+          const text = document.createElement('pre')
+          text.style.cursor = 'text'
+          text.style.userSelect = 'text'
+          text.style.whiteSpace = 'pre-wrap'
+          text.style.margin = '0'
+          text.innerText = ''
+          const [FILES, onFiles] = getFiles()
+          function update(files = FILES) {
+            text.innerText = files.map(({ name, originalText }) => `// @filename:${name}\n${originalText}`).join('\n\n')
+          }
+          update()
+          onFiles(update)
+          this.contentElement.appendChild(text)
+        }
+      }())
     }
   }
-  if (devtools.readyState === 'complete') {
+  if (devtoolsDocument.readyState === 'complete') {
     init()
   } else {
-    devtools.addEventListener('load', init, true)
+    devtoolsDocument.addEventListener('load', init, true)
   }
 })()
