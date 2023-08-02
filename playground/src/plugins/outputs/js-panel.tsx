f91cb076df09b9d8569b6bd7e6e57e3e29d66d87
yijie4188 <yijie4188@gmail.com>
2023-08-02 22:29:54 +0800

    Refactor code highlighter component function

--- playground/src/plugins/outputs/js-panel.tsx
+++ playground/src/plugins/outputs/js-panel.tsx
@@ -1,47 +1,24 @@
-import { useEffect, useMemo, useRef, useState } from 'react'
+import { useMemo } from 'react'
 
 import { ReactRender } from '../index.tsx'
 import { useFiles } from '../../pages/eval-logs/files.ts'
-
-type CodeHighlighter = typeof import('//chii/ui/components/code_highlighter/CodeHighlighter.ts')
+import CodeHighlighter from './code-highlighter.tsx'
 
 export default (function ({ UI, devtoolsWindow: { simport } }) {
-  const [highlightNodeRef, setHighlightNodeRef] = useState<
-    CodeHighlighter['highlightNode'] | undefined
-  >(undefined)
-  simport<CodeHighlighter>('ui/components/code_highlighter/CodeHighlighter.js')
-    .then(({ highlightNode }) => setHighlightNodeRef(() => highlightNode))
-
-  const preEleRef = useRef<HTMLPreElement>(null)
-
   const files = useFiles()
-  const textContent = useMemo(
-    () => files
-      .filter(({ name }) => name.endsWith('.js'))
-      .map(({ name, text, originalText }) => `// @filename:${name}\n${
-        originalText.startsWith('// @devtools.output.compiled\r\n')
-          ? text
-          : originalText
-      }`)
-      .join('\n\n'),
-    [files]
-  )
-  useEffect(() => {
-    const preEle = preEleRef.current
-    if (!preEle) return
-
-    preEle.textContent = textContent
-    highlightNodeRef?.(preEle, 'text/javascript')
-  }, [textContent, highlightNodeRef])
-
-  return <pre
-    ref={preEleRef}
-    style={{
-      cursor: 'text',
-      userSelect: 'text',
-      whiteSpace: 'pre-wrap',
-      margin: '0',
-      padding: '0 4px'
-    }}
+  return <CodeHighlighter
+    code={useMemo(
+      () => files
+        .filter(({ name }) => name.endsWith('.js'))
+        .map(({ name, text, originalText }) => `// @filename:${name}\n${
+          originalText.startsWith('// @devtools.output.compiled\r\n')
+            ? text
+            : originalText
+        }`)
+        .join('\n\n'),
+      [files]
+    )}
+    lang='javascript'
+    devtoolsWindow={{ simport }}
   />
 }) as ReactRender
